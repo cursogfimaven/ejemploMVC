854c9c940986a8592362e64d493d0eb59673107f
gaizka_bilbao <gaizka_bilbao@GFIEJ090.gfinorte.com>
2020-10-29 18:58:21 +0100

    feature-gaizka

--- ejemploMVC/src/main/java/curso/gfi/maven/HomeController.java
+++ ejemploMVC/src/main/java/curso/gfi/maven/HomeController.java
@@ -22,9 +22,9 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/gaizka", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("Welcome gaizka! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -33,7 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "gaizka";
 	}
 	
 }
