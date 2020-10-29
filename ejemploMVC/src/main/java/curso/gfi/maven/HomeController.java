a8921911f3e3534616cd019b2ac7060603905481
gaizka_bilbao <gaizka_bilbao@GFIEJ090.gfinorte.com>
2020-10-29 19:04:09 +0100

    cambio gaizka

--- ejemploMVC/src/main/java/curso/gfi/maven/HomeController.java
+++ ejemploMVC/src/main/java/curso/gfi/maven/HomeController.java
@@ -11,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
+
 /**
  * Handles requests for the application home page.
  */
@@ -19,12 +21,33 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
+
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/gaizka", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome gaizka! The client locale is {}.", locale);
+	public String gaizka(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
